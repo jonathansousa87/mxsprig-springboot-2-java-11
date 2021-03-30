a9734d988ffc27acfa5e3db730d3548048f5b47c
jonathansousa87 <jonathandrumbass@gmail.com>
2021-03-30 17:30:26 -0300

    Project created

---
+++ src/main/java/com/project/mxspring/MxspringApplication.java
@@ -0,0 +1,13 @@
+package com.project.mxspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MxspringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MxspringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/mxspring/MxspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.mxspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MxspringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
